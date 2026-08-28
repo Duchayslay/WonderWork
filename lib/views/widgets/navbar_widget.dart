de3b5684f19335fceafc4107eacf22534dcaa6a7
Duchayslay <hoangvanduc290805@gmail.com>
2026-08-28 16:55:53 +0700

    first home page with pass data selected page

--- lib/views/widgets/navbar_widget.dart
+++ lib/views/widgets/navbar_widget.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:wonderwork/data/notifiers.dart';
 
-class NavbarWidget extends StatefulWidget {
+class NavbarWidget extends StatelessWidget {
   const NavbarWidget({super.key});
 
   @override
-  State<NavbarWidget> createState() => _NavbarWidgetState();
-}
-
-class _NavbarWidgetState extends State<NavbarWidget> {
-  int selectedIndex = 0;
-  @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      destinations: [
-        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-        NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
-      ],
-      onDestinationSelected: (int value) => setState(() {
-        selectedIndex = value;
-      }),
+    return ValueListenableBuilder(
+      valueListenable: selectedPageNotifier,
+      builder: (context, selectedPage, child) {
+        return NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          onDestinationSelected: (int value) {
+            selectedPageNotifier.value = value;
+          },
+          selectedIndex: selectedPage,
+        );
+      },
     );
   }
 }
